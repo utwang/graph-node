4e263a69682c09ce768018eee79f61446658364e
utwang <x2x2tone@gmail.com>
2022-09-22 14:59:29 +0900

    Add build option

--- docker/build.sh
+++ docker/build.sh
@@ -14,13 +14,15 @@ then
     REPO_NAME="Checkout of $(git remote get-url origin) at $(git describe --dirty)"
     BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 fi
-for stage in graph-node-build graph-node graph-node-debug
+# for stage in graph-node-build graph-node graph-node-debug
+for stage in graph-node
 do
     $docker build --target $stage \
             --build-arg "COMMIT_SHA=$COMMIT_SHA" \
             --build-arg "REPO_NAME=$REPO_NAME" \
             --build-arg "BRANCH_NAME=$BRANCH_NAME" \
             --build-arg "TAG_NAME=$TAG_NAME" \
+            --platform linux/amd64 \
             -t $stage \
             -f docker/Dockerfile .
 done
